793af87115cd6d45615ef530f560bb31678c3558
Tom Beckmann <tomjonabc@gmail.com>
2019-11-15 16:49:19 +0100

    make player a rigidbody as well

--- Player.gd
+++ Player.gd
@@ -1,24 +1,21 @@
-extends KinematicBody2D
+extends RigidBody2D
 
-# Declare member variables here. Examples:
 var velocity = Vector2(0, 0)
-const ACCELERATION = 0.8
+const ACCELERATION = 20
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	#velocity = Vector2(0,0)
-	velocity *= 0.99
-	if Input.is_action_pressed("ui_right"):
-		velocity.x += ACCELERATION
-	if Input.is_action_pressed("ui_down"):
-		velocity.y += ACCELERATION
-	if Input.is_action_pressed("ui_left"):
-		velocity.x += -ACCELERATION
-	if Input.is_action_pressed("ui_up"):
-		velocity.y += -ACCELERATION
-	move_and_collide (velocity)
 	pass
+
+func _process(delta):
+	velocity = Vector2(0,0)
+	#velocity *= 0.99
+	if Input.is_action_pressed("ui_right"):
+		velocity.x = ACCELERATION
+	if Input.is_action_pressed("ui_down"):
+		velocity.y = ACCELERATION
+	if Input.is_action_pressed("ui_left"):
+		velocity.x = -ACCELERATION
+	if Input.is_action_pressed("ui_up"):
+		velocity.y = -ACCELERATION
+	add_central_force(velocity)
+	# move_and_collide (velocity)
