0531a094215053c9d76c6088870233932e719b38
Wanda Baltzer <wanda.baltzer@student.hpi.de>
2019-11-15 19:55:13 +0100

    added colors to the shield

--- Player.gd
+++ Player.gd
@@ -6,23 +6,19 @@ var cannon_angle = 0
 var charge_start_time = 0
 
 func _ready():
-	pass	
 	get_node("..//MidiController").connect("note_on", self, "note_on")
 	get_node("..//MidiController").connect("note_off", self, "note_off")
 
 func note_on(pitch, velocity):
-	print(pitch)
 	cannon_angle = (pitch - 48) * 10
 	charge_start_time = OS.get_ticks_msec()
 	
 func note_off(pitch, velocity):
 	var direction = Vector2(0, -100).rotated($Cannon.rotation)
-	print(pitch)
 	var projectile = preload("res://Projectile.tscn").instance()
 	projectile.rotation_degrees = $Cannon.rotation_degrees
 	projectile.position = direction + position
 	get_parent().add_child(projectile)
-	print($Cannon.rotation_degrees)
 	projectile.apply_central_impulse(direction * 2 * max((OS.get_ticks_msec() - charge_start_time) / 200, 1))
 
 func _process(delta):
@@ -39,10 +35,3 @@ func _process(delta):
 		velocity.y = -ACCELERATION
 	add_central_force(velocity)
 	# move_and_collide (velocity)
-
-
-
-
-func _on_MidiController_note_on(pitch, velocity):
-	print(pitch)
-	pass # Replace with function body.
--- Shield.gd
+++ Shield.gd
@@ -3,26 +3,46 @@ extends Node
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var pressedkey = []
+var pressed_keys = []
+var circle_scale = 1.0
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-func _on_midicontroller_note_on(pitch,velocity):
-	pressedkey.append(pitch)
-	if len(pressedkey) == 3:
-		analyzeChords(pressedkey)
-func _on_midicontroller_note_off(pitch,velocity):
-	pressedkey.erase(pitch)
+	get_node("../..//MidiController").connect("note_on", self, "note_on")
+	get_node("../..//MidiController").connect("note_off", self, "note_off")
+	
+func note_on(pitch, velocity):
+	pressed_keys.append(pitch)
+	if len(pressed_keys) == 3:
+		analyze_chords(pressed_keys)
+		
+func note_off(pitch, velocity):
+	pressed_keys.erase(pitch)
+
+func add_circle(color, scale):
+	var circle = Sprite.new()
+	circle.texture = preload("res://assets/circle_white.png")
+	circle.scale = Vector2(scale, scale)
+	circle.modulate = color
+	add_child(circle)
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func analyzeChords (notes):
-	if (notes[2] - notes[1] > 4):
-		notes[2] -= 12
-	if (notes[1] - notes[0] > 4):
-		notes[0] += 12
+func analyze_chords(notes):
+	notes.sort()
+#	if (notes[2] - notes[1] > 4):
+#		notes[2] -= 12
+#	if (notes[1] - notes[0] > 4):
+#		notes[0] += 12
+#   print("Notes: " + str(notes[2] - notes[1]) + " " + str(notes[1] - notes[0]))
 	if (notes[2] - notes[1] == 3):
 		if (notes[1] - notes[0] == 4):
-			$Shield.modulate = Color(0,0,2)
+			# $Shield.modulate = Color(1,0,0)
+			add_circle(Color(1, 0, 0), circle_scale)
+			circle_scale *= 1.5
+			print("Dur")
 	if (notes[2] - notes[1] == 4):
 		if (notes[1] - notes[0] == 3):
-			 $Shield.modulate = Color(0,0,1)
+			 # $Shield.modulate = Color(0,0,1)
+			add_circle(Color(0, 0, 1), circle_scale)
+			circle_scale *= 1.5
+			print("Moll")
 	pass
