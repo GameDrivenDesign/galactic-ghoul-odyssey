97b4497381aac429bef703c152da6e6f588e199f
Wanda Baltzer <wanda.baltzer@student.hpi.de>
2019-11-15 19:13:48 +0100

    added not so tiny rockets, they are actually quite large

--- Player.gd
+++ Player.gd
@@ -2,16 +2,31 @@ extends RigidBody2D
 
 var velocity = Vector2(0, 0)
 const ACCELERATION = 300
+var cannon_angle = 0
+var charge_start_time = 0
 
 func _ready():
-	pass
+	pass	
+	get_node("..//MidiController").connect("note_on", self, "note_on")
+	get_node("..//MidiController").connect("note_off", self, "note_off")
+
+func note_on(pitch, velocity):
+	print(pitch)
+	cannon_angle = (pitch - 48) * 10
+	charge_start_time = OS.get_ticks_msec()
+	
+func note_off(pitch, velocity):
+	var direction = Vector2(0, -100).rotated($Cannon.rotation)
+	print(pitch)
+	var projectile = preload("res://Projectile.tscn").instance()
+	projectile.rotation_degrees = $Cannon.rotation_degrees
+	projectile.position = direction + position
+	get_parent().add_child(projectile)
+	print($Cannon.rotation_degrees)
+	projectile.apply_central_impulse(direction * 2 * max((OS.get_ticks_msec() - charge_start_time) / 200, 1))
 
 func _process(delta):
-	if Input.is_action_just_pressed("ui_accept"):
-		var projectile = preload("res://Projectile.tscn").instance()
-		projectile.position = position
-		get_parent().add_child(projectile)
-	
+	$Cannon.rotation_degrees = lerp($Cannon.rotation_degrees, cannon_angle, delta * 5)
 	velocity = Vector2(0,0)
 	#velocity *= 0.99
 	if Input.is_action_pressed("ui_right"):
